569a1adf6700028a11a254caf220e817aeadaad0
Bagrat Aznauryan <bagrat@aznauryan.org>
2017-11-05 09:39:34 +0400

    Test tel:

---
+++ data.js
@@ -0,0 +1,92 @@
+var data = {
+  "contacts": {
+    "address": "14 apt., 47 bld., 4th block, Davtashen, Yerevan 0054, Armenia",
+    "phone": "+374 55 568908",
+    "email": "bagrat@aznauryan.org",
+    "website": "bagrat.aznauryan.org"
+  },
+  "education": {
+    "academic": [
+      {
+        "degree": "Master of Science",
+        "area": "Computer and Information Science",
+        "from": {
+          "year": 2012,
+          "month": 8
+        },
+        "to": {
+          "year": 2015,
+          "month": 1
+        },
+        "university": "American University of Armenia",
+		"url": "http://cse.aua.am/",
+        "diploma": ""
+      },
+      {
+        "degree": "Bachelor of Science",
+        "area": "Pysics",
+        "from": {
+          "year": 2008,
+          "month": 8
+        },
+        "to": {
+          "year": 2012,
+          "month": 6
+        },
+        "university": "Yerevan State University",
+		"url": "http://ysu.am/faculties/en/Physics",
+        "diploma": ""
+      }
+    ],  
+    "online": [
+      {
+        "name": "Introduction to Kubernetes",
+        "provider": "edX.org, Linux Foundation",
+        "url": "",
+        "year": 2017,
+        "month": 7
+      },
+      {
+        "name": "Data Science and Engineering with Spark",
+        "provider": "edX.org, Linux Foundation",
+        "url": "",
+        "year": 2017,
+        "month": 7
+      }
+    ]
+  },
+  "experience": [
+    {
+      "title": "Software Engineer",
+      "from": {
+        "year": 2012,
+        "month": 8
+      },
+      "to": {
+        "year": 2015,
+        "month": 1
+      },
+      "company": "Sauce Labs Inc.",
+      "url": "https://saucelabs.com",
+      "location": "San Francisco, CA, USA",
+      "info": "Working remotely from Yerevan, Armenia",
+      "description": "doing stuff"
+    },
+    {
+      "title": "Software Engineer",
+      "from": {
+        "year": 2012,
+        "month": 8
+      },
+      "to": {
+        "year": 2015,
+        "month": 1
+      },
+      "company": "Aarki CJSC",
+      "url": "https://saucelabs.com",
+      "location": "San Francisco, CA, USA",
+      "info": "Working remotely from Yerevan, Armenia",
+      "description": "doing stuff"
+    }
+  ]
+}
