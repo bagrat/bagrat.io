790bb25113dcd03acad06f4ce395c6fa9ffe01b3
Bagrat Aznauryan <contact@bagrat.io>
2022-08-22 22:42:03 +0400

    Add HomeView to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import V7View from "../views/V7View.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
     {
       path: "/v7",
       name: "v7-story",
