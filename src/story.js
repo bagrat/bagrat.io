7f14a9300f29fbde93be3d58cbfd61e9545c4b49
Bagrat Aznauryan <contact@bagrat.io>
2022-08-23 14:07:22 +0400

    Shorten the story

--- src/story.js
+++ src/story.js
@@ -3,20 +3,18 @@ export default [
     title: "Init",
     image: "/init-480.gif",
     paragraphs: [
-      'At some point in time, for whatever time is, symbolically denoted as 1991, a lump of organic matter constituting a life-form has been manifested and bound by gravity to a muddy sphere that is called Earth by the same kind of cellular beings called humans. A combination of letters, a nifty abstract invention by the same organic kind, was attached to this new one by the biological creators of that being, commonly referred to as parents, in order to reduce the issues of identification in organized groups of humans called society. This combination of letters is called "a name" and the name is Bagrat (<em>bug-rut</em>, emphasis on <em>"rut"</em>, fortunately not on <em>"bug"</em>).',
-      "Before this Bagrat would disintegrate in an evolutionary cycle of being eaten and pooped by other organic systems, it voluntarily desired to join the V7 Labs team. In doing so, he made this web-site with the modest expectation that it will be viewed by other human beings who have enough association with V7 Labs that grants them power to arrange an interview.",
-      'Since the website is intended to demonstrate <em>"a proven interest in computer science from a young age"</em>, it tells a story back from when Bagrat had his initial encounters with actual computers, since his arguably conscious stage of life. While the next 15 minutes may possibly be the most boring 50 minutes of your life, still, please bear with me...<strike>or panda with me.</strike>',
+      'This is a <strike>short</strike> story about Bagrat (<em>bug-rut</em>, emphasis on <em>"rut"</em>, fortunately not on <em>"bug"</em>) as a programmer. It attempts to reveal more than a resume could.',
+      'It is intended to demonstrate <em>"a proven interest in computer science from a young age"</em>, thus it goes back to when Bagrat had his first encounters with actual computers. And since that was more than 18 years ago, it is written with a modest hope that <strong>V7 Labs</strong> team will take the time read to through it all.',
+      'With that said, please scroll down to get the story going!',
     ],
   },
   {
     title: "Hello Computer",
     image: "/hello-computer-480.gif",
     paragraphs: [
-      "Bagrat got his first computer at home when he was 12. It was a moderate desktop PC enough for his mother to carry some programming work to be done at home. He would sit and watch her write weird text in Visual Basic that would make windows and buttons do some meaningful stuff.",
-      'Over time, patterns emerged in Bagrat\'s mind, motivating him to get a hand or two dirty. With his very basic knowledge of Basic and an idea from the future informatics teacher he wrote a program for the school that would speak out an <a href="https://en.wikipedia.org/wiki/Armenian_language">Armenian</a> word and expect it to be written into a textbox to be spellchecked.',
-      "This won him a waiver from the upcoming Pascal classes in the 8th grade. So he used this free time to learn C/C++ to later be used in programming olympiads.",
-      "Before starting the 8th grade, Bagrat spent the summer working in a local electronics store, assembling hundreds of desktop PCs. One day he attempted to insert the RAM into the motherboard the other way around, and feeling resistance he applied more force onto it. The result was broken RAM and motherboard socket.",
-      "This taught him that if something requires brute force intervention, it most probably means something is wrong about the approach in the first place. This applied in many other aspects of life in future and turned out to prove itself.",
+      "Bagrat was 12 when a computer appeared at their home, a moderate PC enough for his mother to carry home some programming work. He watched her write weird text in Visual Basic that would make windows and buttons do some meaningful stuff.",
+      'Over time, patterns emerged in Bagrat\'s mind, motivating him to get a hand or two dirty. With his very basic knowledge of Basic and an idea from the future informatics teacher he wrote a program that would speak out a word and expect it to be written into a textbox to be spellchecked. This won him a waiver from the upcoming Pascal classes in the 8th grade, so he used this free time to learn C/C++ to later be used in programming olympiads.',
+      'The same summer, Bagrat worked in a local electronics store, assembling hundreds of PCs. Once he attempted to insert a RAM the other way around, and feeling resistance he applied more force, breaking it along with the motherboard socket. He realized that a need for brute force usually indicates faulty approach in the first place.'
     ],
   },
   {
@@ -24,7 +22,7 @@ export default [
     image: "/the-way-480.gif",
     paragraphs: [
       "Now Bagrat was excited to find more problems he could use programming to solve. Among a few, he wrote an Excel VBA app for the same electronics store to compare the numbers in accounting and inventory records. Here he faced the limitations of Excel as a data structure without even having the concept of data structure in his vocabulary.",
-      "Later, during his first year of Physics undergraduate studies at the age of 17, he learnt C#, which offered a GUI designer similar to Visual Basic and syntax close enough to C/C++. He used it to write a GUI program for simple inventory and POS operations management for a pharmacy. Even though it worked, it was a brittle collection of GUI component event handlers making direct SQL queries.",
+      "Later, during his first year of Physics undergraduate studies at the age of 17, he learnt C# to write a GUI program for simple inventory and POS operations management for a pharmacy. Even though it worked, it was a brittle collection of GUI component event handlers making direct SQL queries.",
       "The best lesson here for him was that the business processes tend to evolve, thus changing the requirements and problem definitions over time, and so the code needs to account for that.",
     ],
   },
@@ -32,9 +30,9 @@ export default [
     title: "Top-Down",
     image: "/top-down-480.gif",
     paragraphs: [
-      "The passion for programming gave Bagrat some popularity among his more academically-inclined peers. This led to a class-mate referring Bagrat to her uncle, running a local business developing a proprietary wireless transceiver, who was looking for cheap labor, otherwise called interns.",
-      'At his 18, Bagrat got this job where he initially used C# to develop a GUI application to tweak the configuration of the transceiver. He then gradually moved more towards the transceiver side, writing C code on <a href="https://en.wikipedia.org/wiki/AVR32">AVR32 microcontrollers</a>.',
-      'As one of his major projects he integrated a 3rd-party Wi-Fi chip onto the modem as a wireless alternative to <a href="https://en.wikipedia.org/wiki/RS-232">RS-232</a> for data transmission and configuration. While this might sound fancy, it was essentially calling the right custom functions from inside the right Wi-Fi library callbacks. But the important side of this is that it required Bagrat to learn and understand the <a href="https://en.wikipedia.org/wiki/OSI_model">OSI model</a>.',
+      "The passion for programming gave Bagrat some popularity among his more academically-inclined peers. This led to a class-mate referring Bagrat to her uncle, running a local business developing a proprietary wireless transceiver.",
+      'At his 18, Bagrat got this job where he started developing a C# GUI application to configure the transceiver over <a href="https://en.wikipedia.org/wiki/RS-232">RS-232</a>. He then gradually moved towards the lower-level, writing C code on <a href="https://en.wikipedia.org/wiki/AVR32">AVR32 microcontrollers</a>.',
+      'As one of his major projects he integrated a 3rd-party Wi-Fi chip onto the modem. While this might sound fancy, it was essentially about calling the right custom functions from inside the right callbacks. But more importantly it required Bagrat to learn and understand the <a href="https://en.wikipedia.org/wiki/OSI_model">OSI model</a>.',
       "Even though this world of embedded software was extremely interesting for him, it was rigid and wouldn't allow fast experiments. So Bagrat started learning OOP and Java to head towards the Web.",
     ],
   },
@@ -43,9 +41,9 @@ export default [
     image: "/bottom-up-480.gif",
     paragraphs: [
       "In 2012 Bagrat graduated with a Physics bachelor's degree and started master's degree in Computer and Information Science. At the same time he landed a new job as a Java back-end developer for a <a href=\"https://www.be2.com/\">matchmaking website company</a>.",
-      "At this job he was mainly implementing new controllers and fixing bugs. But more importantly he got to use version control and wrote tests for the first time, which were satisfying solutions to issues he had faced first-hand at his previous projects. Here he also got introduced to Agile, Scrum specifically.",
-      "In parallel, the graduate studies took all the practical knowledge Bagrat had, combined with the new theoretical ones, and properly categorized them all, putting everything into appropriate mental shelves. Now he had a solid map to navigate giving him efficiency of looking for solutions in the right areas.",
-      "At his next job, the requirements gathering for e-commerce and government apps were going pretty slowly, which was giving Bagrat a lot of free time to learn and experiment with continuous integration, write shell scripts for deployment and take number of online courses, one of which was the famous <em>Stanford Machine Learning course by Andrew Ng</em>.",
+      "He got to use version control and wrote tests for the first time, which were satisfying solutions to issues he had faced first-hand previously. Here he also got introduced to Agile, Scrum specifically.",
+      "In parallel, the graduate studies took Bagrat's practical knowledge, wrapped in theoretical fundamentals and properly categorized them all into appropriate mental shelves. Now he had a solid map to navigate, allowing him to look for solutions in the right areas.",
+      "At his next job Bagrat spent most of the time to learn and experiment with continuous integration, write shell scripts for deployment and take number of online courses, one of which was the famous <em>Stanford Machine Learning course by Andrew Ng</em>.",
     ],
   },
   {
@@ -82,9 +80,9 @@ export default [
     title: "One of the two hard problems",
     image: "/naming-480.gif",
     paragraphs: [
-      "After 5 years at Sauce Labs, Bagrat's former manager offered a job in a corporate lunch delivery company which was struggling after a COVID-lockdown-driven layover and needed to adapt to new circumstances by redoing some software.",
-      "Here he witnessed how catastrophically off could naming of domain concepts ever be done. The least problem of it was that discussions during meetings would not make any sense to a person new to it all. But the worst part was that when you start to make sense, it would erode your thinking so badly that you would be caught in the trap of not being able to produce any new sensible work going forwards.",
-      "This taught Bagrat to be generous for the time dedicated to naming, which will pay off in multiples later. Being underpowered on staff and having to spend most of the time patching 3 different, chronologically sequential legacy codebases in JavaScript to keep the critical operations going, he eventually gave up and moved on.",
+      "After 5 years at Sauce Labs, Bagrat's former manager offered a job in a corporate lunch delivery company which needed to update some legacy software written in JavaScript to adapt to new circumstances created by COVID.",
+      'Here he witnessed the most confusing naming of domain concepts. The actual challenge of it though was that after understanding the concepts and thinking in those terms, it was nearly impossible to add new work in that context that would make sense. Being underpowered on staff and having to spend most of the time patching the legacy to keep the critical operations going, he eventually gave up and moved on.',
+      "This taught Bagrat to be generous for the time dedicated to naming, which will pay off in multiples later.",
     ],
   },
   {
