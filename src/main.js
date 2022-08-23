a74c3b6ec7ca4db569fe9c906953f632a6d39c15
Bagrat Aznauryan <contact@bagrat.io>
2022-08-23 20:28:31 +0400

    Add analytics

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import VueGtag from "vue-gtag";
+
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueGtag, {
+  config: { id: "G-HNLFGWWDBJ" }
+})
 
 app.mount("#app");
